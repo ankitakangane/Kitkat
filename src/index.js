6c5c22a2e840c52c0ca0f814db8aaa01c27ba525
ankitakangane <kanganeankita1@gmail.com>
2024-08-09 06:36:24 +0530

    use component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 
-const root =createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
+function Cadburry(){
+    return <h3>Kitkat</h3>
+}
 
 root.render(
-    <>
-<h1>Kitkat</h1>
+    
+  <>
+  <h1>All Cadburry</h1>
+  <Cadburry/>
+    
+           {/*<h1>Kitkat</h1>
 <h2>Price:{5+5}</h2>
 <hr/>
 <p>KIT KAT® Bars have long been associated with breaktime. The "break" tagline was first introduced in 1957, then later evolving into the iconic "Gimme A Break" jingle in 1988.
 </p>
 <hr/>
 
-   </>
+*/}
+    </>
 );
