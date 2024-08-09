70e5e29ec7b2d3fbb5f4b700be73013076ad1eb3
ankitakangane <kanganeankita1@gmail.com>
2024-08-09 08:40:42 +0530

    use styling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
+import {Cadburry,CadburryKitkat} from "./App.js";
 
 const root = createRoot(document.getElementById("root"));
 {/*
@@ -8,16 +9,19 @@ function Cadburry(){
 }
     */ }
     // Arrow Function
+    {/* 
     const Cadburry= (props) =>{
         console.log(props);
         return <h3>{props.name}-{props.price}</h3>
     }
+        */}
 
 root.render(
     
   <>
   <h1>All Cadburry</h1>
   <Cadburry name="kitkat" price="20"/>
+  <CadburryKitkat/>
     
            {/*<h1>Kitkat</h1>
 <h2>Price:{5+5}</h2>
