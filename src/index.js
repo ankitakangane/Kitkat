993488f44fe7df91b857a9c73d945bac2cc6d403
ankitakangane <kanganeankita1@gmail.com>
2024-08-09 06:24:55 +0530

    print the cadburry and its info

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import {createRoot} from "react-dom/client";
+
+const root =createRoot(document.getElementById("root"));
+
+root.render(
+    <>
+<h1>Kitkat</h1>
+<h2>Price:{5+5}</h2>
+<hr/>
+<p>KIT KAT® Bars have long been associated with breaktime. The "break" tagline was first introduced in 1957, then later evolving into the iconic "Gimme A Break" jingle in 1988.
+</p>
+<hr/>
+
+   </>
+);
